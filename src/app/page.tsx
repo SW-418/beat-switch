24aac429d934f443b5300fe2278709a48e880526
Sam Wells <samwells95@gmail.com>
2025-04-08 20:28:26 -0700

    Add blurb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useState } from "react";
 import NavigationBar from "./components/navigation-bar";
 
 export default function Home() {
@@ -10,6 +9,7 @@ export default function Home() {
         <NavigationBar />
       </div>
       <h1 className="text-4xl font-bold mb-8"> Beat Switch </h1>
+      <h2 className="text-2xl font-bold mb-8"> Generate playlists and sync tracks between Spotify and Apple Music </h2>
     </div>
   );
 }
