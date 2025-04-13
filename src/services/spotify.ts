b531c162ddf7ea7478f07d729746f682bb56f4f0
Sam Wells <samwells95@gmail.com>
2025-04-13 11:45:12 -0700

    Get all playlists in svc

--- src/services/spotify.ts
+++ src/services/spotify.ts
@@ -1,6 +1,7 @@
 import SpotifyUserApiClient from "@/data-access/spotify/users";
 import { Track } from "./track";
-import { SavedTrackObject, UsersTopTracksResponse, CreatePlaylistResponse, AddTracksToPlaylistResponse } from "spotify-api";
+import { SavedTrackObject, UsersTopTracksResponse, CreatePlaylistResponse, AddTracksToPlaylistResponse, ListOfCurrentUsersPlaylistsResponse, PlayListTrackObject } from "spotify-api";
+import { Playlist } from "@/app/types/responses/playlist";
 
 class SpotifyService implements IMusicService {
   constructor() { }
@@ -49,12 +50,46 @@ class SpotifyService implements IMusicService {
     }));
   }
 
+  mapPlaylists(playlists: ListOfCurrentUsersPlaylistsResponse[]): Playlist[] {
+    return playlists.map(p => ({
+      id: p.id,
+      name: p.name,
+      description: p.description
+    }));
+  }
+
+  mapPlaylistTracks(tracks: PlayListTrackObject[]): Track[] {
+    return tracks.map(t => ({
+      id: t.track.id,
+      name: t.track.name,
+      artists: t.track.artists,
+      album: t.track.album.name,
+      isrc: t.track.external_ids.isrc,
+      added_at: t.added_at
+    }));
+  }
+
   async getSavedTracks(accessToken: string, limit: number, offset: number): Promise<Track[]> {
     return await SpotifyUserApiClient.getTracks(accessToken, limit, offset).then(t => this.mapSavedTracks(t.items));
   }
 
   async getTopTracks(accessToken: string, limit: number, offset: number): Promise<Track[]> {
     return await SpotifyUserApiClient.getTopTracks(accessToken, limit, offset).then(t => this.mapTopTracks(t.items));
+  }
+
+  async getPlaylists(accessToken: string): Promise<Playlist[]> {
+    return await SpotifyUserApiClient.getPlaylists(accessToken).then(t => this.mapPlaylists(t.items));
+  }
+
+  async getAllPlaylistTracks(accessToken: string, playlistId: string): Promise<Track[]> {
+    const tracks = new Array<PlayListTrackObject>();
+    let hasNextPage = true;
+    do {
+      const playlistTracksResponse = await SpotifyUserApiClient.getPlaylistTracks(accessToken, playlistId, 50, tracks.length);
+      tracks.push(...this.mapPlaylistTracks(playlistTracksResponse.items));
+      hasNextPage = playlistTracksResponse.next !== null;
+    } while (hasNextPage);
+    return tracks;
   }
 
   async createPlaylist(accessToken: string, name: string, userId: string, description: string): Promise<CreatePlaylistResponse> {
@@ -73,6 +108,8 @@ interface IMusicService {
   getAllSavedTracks(accessToken: string): Promise<Track[]>;
   getSavedTracks(accessToken: string, limit: number, offset: number): Promise<Track[]>;
   getTopTracks(accessToken: string, limit: number, offset: number): Promise<Track[]>;
+  getPlaylists(accessToken: string): Promise<Playlist[]>;
+  getAllPlaylistTracks(accessToken: string, playlistId: string): Promise<Track[]>;
   createPlaylist(accessToken: string, name: string, userId: string, description: string): Promise<CreatePlaylistResponse>;
   addTracksToPlaylist(accessToken: string, playlistId: string, uris: string[]): Promise<AddTracksToPlaylistResponse>;
 }
