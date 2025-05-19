3bdd93695daed6575103cc0a48a2b16142e4ecd7
Sam Wells <samwells95@gmail.com>
2025-05-19 16:32:50 -0700

    Add logic to create Songs and Artists in DB

--- src/data-access/db/song.ts
+++ src/data-access/db/song.ts
@@ -1,22 +1,37 @@
+import { Track } from "@/services/track";
 import { PrismaClient } from "@prisma/client";
+import HashGenerator from "@/data-access/hash-generator";
 
 class SongDb {
     private prisma: PrismaClient;
+    private hashGenerator: HashGenerator;
+
     constructor() {
         this.prisma = new PrismaClient();
+        this.hashGenerator = new HashGenerator();
     }
 
-    async createSong(name: string, artist: string, album: string, releaseDate: string, isrc: string, duration: number): Promise<number> {
-        // TODO: Need to check if song already exists by checking ISRC, name, artist, album
-        const song = await this.prisma.song.create({
-            data: {
-                name,
-                artist,
-                album,
-                releaseDate,
-                isrc,
-                duration
-            }
+    async createSong(track: Track): Promise<number> {
+        const hash = await this.hashGenerator.generateTrackHash(track);
+        const song = await this.prisma.song.upsert({
+            where: {
+                hash: hash
+            },
+            create: {
+                name: track.name,
+                album: track.album,
+                releaseDate: track.releaseDate,
+                isrc: track.isrc,
+                duration: track.duration,
+                hash: hash,
+                artists: {
+                    connectOrCreate: track.artists.map(artistName => ({
+                        where: { name: artistName },
+                        create: { name: artistName }
+                    }))
+                }
+            },
+            update: {}
         });
         return song.id;
     }
