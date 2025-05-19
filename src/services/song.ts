ec1ce5bd58e3f1f4c6b5a5f8db01112fdecb3e05
Sam Wells <samwells95@gmail.com>
2025-05-19 16:33:33 -0700

    Update song svc to use Track

--- src/services/song.ts
+++ src/services/song.ts
@@ -1,4 +1,5 @@
 import SongDb from "@/data-access/db/song";
+import { Track } from "./track";
 
 class SongService {
     private songDb: SongDb;
@@ -7,8 +8,8 @@ class SongService {
         this.songDb = new SongDb();
     }
 
-    async createSong(name: string, artist: string, album: string, releaseDate: string, isrc: string, duration: number): Promise<number> {
-        return await this.songDb.createSong(name, artist, album, releaseDate, isrc, duration);
+    async createSong(track: Track): Promise<number> {
+        return await this.songDb.createSong(track);
     }
 }
 
